a52f87bfbd8f376daac7a33437b8d8caaae6733c
Lee In Seok <dlstjr9068@gmail.com>
2026-03-06 00:50:54 +0900

    코드 수정

--- apps/sample/pb_hooks/pages/(site)/+load.js
+++ apps/sample/pb_hooks/pages/(site)/+load.js
@@ -1,6 +1,6 @@
 /** @type {import('pocketpages').PageDataLoaderFunc} */
 module.exports = ({ meta }) => {
-  meta('title', 'About Us')
+  meta('title', 'About')
   meta('description', 'Learn more about our company and mission')
   meta('image', 'https://example.com/about-preview.jpg')
 
