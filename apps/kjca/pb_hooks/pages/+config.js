894915a85e68e450eaa1d2907115969d61e650f1
Lee In Seok <dlstjr9068@gmail.com>
2026-03-16 19:40:20 +0900

    디버그 추가

--- apps/kjca/pb_hooks/pages/+config.js
+++ apps/kjca/pb_hooks/pages/+config.js
@@ -1,3 +1,9 @@
-module.exports = {
-  plugins: ['pocketpages-plugin-js-sdk', 'pocketpages-plugin-auth', 'pocketpages-plugin-ejs', 'pocketpages-plugin-realtime'],
+module.exports = function (api) {
+  const appEnv = String(api.env('APP_ENV') || 'development').trim();
+  const isDevelopment = appEnv === 'development';
+
+  return {
+    plugins: ['pocketpages-plugin-js-sdk', 'pocketpages-plugin-auth', 'pocketpages-plugin-ejs', 'pocketpages-plugin-realtime'],
+    debug: isDevelopment,
+  };
 };
--- apps/sample/pb_hooks/pages/+config.js
+++ apps/sample/pb_hooks/pages/+config.js
@@ -1,3 +1,9 @@
-module.exports = {
-  plugins: ['pocketpages-plugin-ejs', 'pocketpages-plugin-realtime'],
+module.exports = function (api) {
+  const appEnv = String(api.env('APP_ENV') || 'development').trim();
+  const isDevelopment = appEnv === 'development';
+
+  return {
+    plugins: ['pocketpages-plugin-ejs', 'pocketpages-plugin-realtime'],
+    debug: isDevelopment,
+  };
 };
