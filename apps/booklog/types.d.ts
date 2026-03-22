3054269c38762c333597593dc316d454dc8e8a43
Lee In Seok <dlstjr9068@gmail.com>
2026-03-22 18:51:16 +0900

    업로드 로직 보강

--- apps/booklog/types.d.ts
+++ apps/booklog/types.d.ts
@@ -1,3 +1,23 @@
 declare function require(moduleName: string): any;
 
-declare namespace types {}
+declare namespace types {
+  interface BookSearchScoreDetails {
+    titleExact: boolean
+    titlePartial: boolean
+    authorExact: boolean
+    authorPartial: boolean
+    translatorExact: boolean
+    translatorPartial: boolean
+    publisherExact: boolean
+    publisherPartial: boolean
+    isbnExact: boolean
+    hasIsbn13: boolean
+    titleScore: number
+    authorScore: number
+    translatorScore: number
+    publisherScore: number
+    isbnScore: number
+    bonusScore: number
+    score: number
+  }
+}
