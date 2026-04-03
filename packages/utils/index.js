f065994bfab3e1ba8fbc28b8afbf94aa2afee288
Lee In Seok <dlstjr9068@gmail.com>
2026-04-03 18:09:28 +0900

    코드 정리

--- packages/utils/index.js
+++ packages/utils/index.js
@@ -2,14 +2,6 @@
 
 const dateutil = require('./dateutil');
 
-/**
- * utils 패키지 연결 상태를 간단히 확인합니다.
- */
-function logUtilsReady() {
-  console.log('@pocketpages/utils ready');
-}
-
 module.exports = {
   dateutil,
-  logUtilsReady,
 };
