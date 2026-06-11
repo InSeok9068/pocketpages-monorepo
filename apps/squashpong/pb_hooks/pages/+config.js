983aa22e824c268f671a27cd06f4a8edb3538b4b
Lee In Seok <dlstjr9068@gmail.com>
2026-06-11 10:00:17 +0900

    Archive squashpong app

--- apps/squashpong/pb_hooks/pages/+config.js
+++
@@ -1,9 +0,0 @@
-module.exports = function (api) {
-  const appEnv = String(api.env('APP_ENV') || 'development').trim()
-  const isDevelopment = appEnv === 'development'
-
-  return {
-    plugins: ['pocketpages-plugin-ejs'],
-    debug: isDevelopment,
-  }
-}
